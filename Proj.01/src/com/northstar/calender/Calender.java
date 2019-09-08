251105a43256a7f95ccdce8ed983f336acc7db26
Iris <Iris@Iris_Northstar>
2019-09-09 00:12:35 +0900

    A new file

---
+++ Proj.01/src/com/northstar/calender/Calender.java
@@ -0,0 +1,11 @@
+package com.northstar.calender;
+
+public class Calender {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi, Calender!");
+
+	}
+
+}
